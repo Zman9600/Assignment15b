b721658231ba7e46994ac251f0d6f302e45ec21c
Zman9600 <40652111+Zman9600@users.noreply.github.com>
2021-12-01 15:09:01 -0500

    Defined half() functions, added tests

--- main.cpp
+++ main.cpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 template <typename T>
 T half(T);
+
 int half(int);
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+}
+
+template <typename T>
+T half(T input)
+{
+    return input / 2;
+}
+
+int half(int input)
+{
+    return round(input / 2.0f);
 }
